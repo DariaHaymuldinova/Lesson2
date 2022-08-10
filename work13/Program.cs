7863db0b6f9524f60da1ad7c2f98a1a619038433
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-10 10:57:01 +0400

    Добавили 13 задачу

---
+++ work13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа: Выводит третью цифру заданного числа
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberString = number.ToString();
+if (number > 99)
+{
+    string result = numberString[2].ToString();
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+

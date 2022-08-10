39cbfe766158ebbb0c8fa5e4f1b351c7833ff459
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-10 11:26:54 +0400

    Добавили 15 задачу

---
+++ work15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа: принимает на вход цифру обозначающую день недели и проверяет является ли этот день выходным
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+if ((number > 5) && (number < 8))
+{
+    Console.WriteLine("да");
+}
+
+else if (number > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
+Console.ReadLine();

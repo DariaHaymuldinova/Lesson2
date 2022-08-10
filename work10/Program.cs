8052e837f5c339f059000eb3a0ca0478b7a0b870
dasha.lisikhina@yandex.ru <dasha/lisikhina@yandex.ru>
2022-08-10 08:11:38 +0400

    Добавили 10 задачу

---
+++ work10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа: Показывает вторую цифру трехзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+string numberString = number.ToString();
+string result = numberString[1].ToString();
+Console.WriteLine(result);
